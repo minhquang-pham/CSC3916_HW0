586ec3fd1693ed3bcfe08ddbf0cc45224874422f
minhquang-pham <minhquang.pham@ucdenver.edu>
2021-01-31 16:30:55 -0700

    Add files via upload

---
+++ utils/googlebooks.js
@@ -0,0 +1,35 @@
+const axios = require('axios')
+
+module.exports = async (phrase) => {
+    const results = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+        params: {
+            format: 'json',
+            q: `${phrase}`
+        }
+    })
+
+    //return JSON.stringify(results.data);
+    let myObject = {
+        data: response.data,
+        status: response.status,
+        statusText: response.statusText,
+        headers: response.headers,
+        requestHeader: response.config.headers
+    }
+
+    return myObject;
+}
+
+/*  console.log(results.data);
+    console.log(results.status);
+    console.log(results.statusText);
+    console.log(results.headers);
+
+        const results = await axios({
+        method: 'get',
+        url: 'https://www.googleapis.com/books/v1/volumes',
+        params: {
+            format: 'json',
+            q: `${phrase}`,
+        },
+    })*/
